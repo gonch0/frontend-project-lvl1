726c17f39a158c3e8c78228edfec82e3331ccc5a
ivan <givan1010@gmail.com>
2021-01-05 18:21:02 +0500

    plugins

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+
+export default function askName() {
+
+        const name = readlineSync.question('May I have your name? ');
+    console.log("Hello, " + name + "!");
+
+
+}
+
+
